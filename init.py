105df22a8afc43f04d17ea76344569a1595e19cb
Nezio <nemanjaz1994@gmail.com>
2020-04-05 14:53:16 +0200

    Add toast notification
    Added toast notification on all tasks complete.

--- init.py
+++ init.py
@@ -1,10 +1,21 @@
 import main
 
+from win10toast import ToastNotifier
+
 # configs
 import configs.default as config_default
 
 def init():
+    
     main.run(config_default)
+
+
+
+
+    # notify Windows that all tasks are done
+    toaster = ToastNotifier()
+    toaster.show_toast("Facial Expression Recognition","All tasks completed!")
+
 
 
 
